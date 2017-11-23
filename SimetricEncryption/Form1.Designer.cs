885cb9d64f80c2b0755638176ff9b595d25a2628
jpd21122012 <slipknot_jpd@hotmail.com>
2017-11-22 22:42:25 -0600

    Addition of SimetricEncryptionClass using rijndael object.

---
+++ SimetricEncryption/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SimetricEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(423, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key: ";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(524, 39);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(478, 31);
+            this.tbKey.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Message:";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(173, 186);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(478, 252);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(833, 186);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(478, 252);
+            this.tbResult.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(701, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(328, 491);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(187, 62);
+            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(995, 491);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(187, 62);
+            this.btnDecrypt.TabIndex = 7;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 676);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ SimetricEncryption/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace SimetricEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            String message = tbResult.Text;
+            String password = tbKey.Text;
+            byte[] textByte;
+            textByte = Encoding.Default.GetBytes(message);
+            String result = TextDecryption(textByte, (new PasswordDeriveBytes(password, null)).GetBytes(32));
+            tbMessage.Text = result;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            String message = tbMessage.Text;
+            String password = tbKey.Text;
+            String result = TextEncryption(message,(new PasswordDeriveBytes(password,null)).GetBytes(32));
+            tbResult.Text = result;
+        }
+        string TextEncryption(string message, byte[] key)
+        {
+            Rijndael rijndael = Rijndael.Create();
+            byte[] encryptByte = null;
+            byte[] outputByte = null;
+            try
+            {
+                rijndael.Key = key;
+                rijndael.GenerateIV();
+                byte[] inputByte = Encoding.UTF8.GetBytes(message);
+                encryptByte = rijndael.CreateEncryptor().TransformFinalBlock(inputByte, 0, inputByte.Length);
+                outputByte = new byte[rijndael.IV.Length + encryptByte.Length];
+                rijndael.IV.CopyTo(outputByte, 0);
+                encryptByte.CopyTo(outputByte, rijndael.IV.Length);
+                ;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            finally
+            {
+                rijndael.Clear();
+            }
+            string output = Encoding.Default.GetString(outputByte);
+            return output;
+        }
+        string TextDecryption(byte[] cryptedData, byte[] key)
+        {
+            string decryptedText=string.Empty;
+            Rijndael rijndael = Rijndael.Create();
+            byte[] temp = new byte[rijndael.IV.Length];
+            byte[] cryptedByte = new byte[cryptedData.Length-rijndael.IV.Length];
+
+            try
+            {
+                rijndael.Key = key;
+                Array.Copy(cryptedData,temp,temp.Length);
+                Array.Copy(cryptedData,temp.Length, cryptedByte,0,cryptedByte.Length);
+                rijndael.IV = temp;
+                decryptedText = Encoding.UTF8.GetString((rijndael.CreateDecryptor()).TransformFinalBlock(cryptedByte,0,cryptedByte.Length));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            finally
+            {
+                rijndael.Clear();
+            }
+            return decryptedText;
+        }
+    }
+}
